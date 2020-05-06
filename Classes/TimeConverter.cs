e03b52e65abf6eb2e26a88c42435f5106a45dedd
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2020-05-06 11:21:27 +0200

    all tests green

--- Classes/TimeConverter.cs
+++ Classes/TimeConverter.cs
@@ -7,23 +7,23 @@ namespace BerlinClock
     {
         public string ConvertTime(string time)
         {
-            var dateTime = DateTime.Parse(time);
+            var dateTime = this.Parse(time);
 
-            var hours = dateTime.Hour;
+            var hours = dateTime.Hours;
             var h5Lamps = hours / 5;
             var h1Lamps = hours % 5;
 
-            var minutes = dateTime.Minute;
+            var minutes = dateTime.Minutes;
 
             var m5Lamps = minutes / 5;
             var m1Lamps = minutes % 5;
 
-            var sLamp = dateTime.Second % 2;
+            var sLamp = dateTime.Seconds % 2;
 
             var stringBuilder = new StringBuilder();
 
             stringBuilder.AppendLine(sLamp == 0 ? "Y" : "O");
-            
+
             for (int i = 0; i < h5Lamps; i++)
             {
                 stringBuilder.Append("R");
@@ -50,7 +50,7 @@ namespace BerlinClock
 
             for (int i = 0; i < m5Lamps; i++)
             {
-                stringBuilder.Append("Y");
+                stringBuilder.Append((i+1) % 3 == 0 ? "R" : "Y");
             }
 
             for (int i = 11; i > m5Lamps; i--)
@@ -73,5 +73,25 @@ namespace BerlinClock
 
             return stringBuilder.ToString();
         }
+
+        private BerlinTimeSpan Parse(string time)
+        {
+            var sections = time.Split(new[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
+            return new BerlinTimeSpan(int.Parse(sections[0]), int.Parse(sections[1]), int.Parse(sections[2]));
+        }
+    }
+
+    public class BerlinTimeSpan
+    {
+        public BerlinTimeSpan(int hours, int minutes, int seconds)
+        {
+            this.Hours = hours;
+            this.Minutes = minutes;
+            this.Seconds = seconds;
+        }
+
+        public int Hours { get;  }
+        public int Minutes { get;  }
+        public int Seconds { get;  }
     }
 }
